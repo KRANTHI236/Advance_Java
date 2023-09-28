3727db08228bda8c39eba2b95efd5f4990f70bd7
kiranr <kiranr@alaptop-14.sathguru.local>
2023-09-28 15:43:32 +0530

    first commit.

---
+++ ProductAndImagesInsertingByUsingJdbc/src/main/java/com/sathguru/main/FetchingImage.java
@@ -0,0 +1,60 @@
+package com.sathguru.main;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FetchingImage {
+
+	public static void main(String[] args) {
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String dburl = "jdbc:oracle:thin:@//10.0.0.5:1521/rvpdb1.sub08140900360.rvtestvcn.oraclevcn.com";
+			String dbun = "TEST_ALL";
+			String dbpwd = "test_all";
+
+			Connection connection = DriverManager.getConnection(dburl, dbun, dbpwd);
+
+			PreparedStatement preparedStatement = connection.prepareStatement("Select * from IMAGE_OR_FILE");
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+//			if (resultSet.next()) {
+//				Blob blob = resultSet.getBlob(4);
+//				byte b[] = blob.getBytes(253, (int) blob.length());
+//
+//				FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\kiranr\\Pictures\\River.jpg");
+//
+//				fileOutputStream.write(b);
+//
+//				fileOutputStream.close();
+//			}
+
+			while (resultSet.next()) {
+				Long id = resultSet.getLong("id");
+				String name = resultSet.getString("name");
+				String description = resultSet.getString("description");
+
+				Blob image = resultSet.getBlob("image");
+
+				System.out.println(id + "\t\t" + name + "\t\t" + description+"\t\t"+image);
+
+			}
+
+			System.out.println("fetched successfully");
+			connection.close();
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
