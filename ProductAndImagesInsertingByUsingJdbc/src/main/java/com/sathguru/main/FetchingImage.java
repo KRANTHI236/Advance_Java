ea3720e63aabb3260b0fc0111bc1fe8d938d1568
Kranthi Kiran <rachhakranthikiran@gmail.com>
2023-10-09 17:27:05 +0530

    Update FetchingImage.java

--- ProductAndImagesInsertingByUsingJdbc/src/main/java/com/sathguru/main/FetchingImage.java
+++ ProductAndImagesInsertingByUsingJdbc/src/main/java/com/sathguru/main/FetchingImage.java
@@ -17,7 +17,7 @@ public class FetchingImage {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			String dburl = "jdbc:oracle:thin:@//10.0.0.5:15212/rvpdb1.sub08140900360.rvtestvcn.oraclevcn.com";
+			String dburl = "jdbc:oracle:thin:@//10.0.0.5:1521/rvpdb1.sub081409003601.rvtestvcn.oraclevcn.com";
 			String dbun = "TEST_ALL";
 			String dbpwd = "test_all";
 
